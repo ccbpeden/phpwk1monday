2bb1f67c5d04ac4aa814e354cf34117c79d4dcb7
Charles Peden <ccbpeden@warpmail.net>
2017-02-06 10:22:14 -0800

    encapsulate object, add getters & setters

--- car.php
+++ car.php
@@ -1,20 +1,44 @@
 <?php
 class Car
 {
-    public $make_model;
-    public $price;
-    public $miles;
+    private $make_model;
+    private $price;
+    private $miles;
+
+    function __construct($make_model, $price, $miles)
+    {
+      $this->make_model = $make_model;
+      $this->price = $price;
+      $this->miles = $miles;
+    }
 
     function worthBuying($max_price)
     {
         return $this->price < ($max_price + 100)
     }
 
-    function __construct($make_model, $price, $miles)
+    function setPrice($new_price)
     {
-        $this->make_model = $make_model;
-        $this->price = $price;
-        $this->miles = $miles;
+        $float_price = (float) $new_price;
+        if ($float_price != 0) {
+          $formatted_price = number_format($float_price, 2);
+            $this->price = $formatted_price;
+        }
+    }
+
+    function getPrice()
+    {
+        return $this->price;
+    }
+
+    function getMake_model()
+    {
+        return $this->make_model;
+    }
+
+    function getMiles()
+    {
+        return $this->miles;
     }
 }
 
